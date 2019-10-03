d58829cc33eef5c9527707fb1fe0879a00555c0e
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2019-10-03 11:00:41 -0400

    set up basic HTML layout

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 //Start with a word and turn into an array
-word = "frighten".split("")
-answerArray = ["_"] * word.length
+word = "frighten".split("");
+answerArray = ["_"] * word.length;
 
 //Display alphabet options
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+const buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (let i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
