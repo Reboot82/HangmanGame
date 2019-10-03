bfd4cc01cb5eacb4eb2f5c8d577d0bc91ad0b63f
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2019-10-03 10:42:43 -0400

    added image and started JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+//Start with a word and turn into an array
+word = "frighten".split("")
+answerArray = ["_"] * word.length
+
+//Display alphabet options
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
