256c3206c7dafb11a4c76cf647333f719266bc59
Brandt Campbell <brandtcampbell@Brandts-MacBook-Air.local>
2019-10-04 20:16:09 -0400

    i think it works...

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ function newGame() {
     numWrong = 0;
     numRight = 0;
     answerArray = [];
+    hngmn.clearRect(0, 0, canvas.width, canvas.height);
     $('.answer').remove();
     $('.buttons').remove();
     pickWord();
+    hangman();
 };
 
 //Create array of word options
@@ -59,17 +61,33 @@ function pickWord() {
                     numRight++;
                 }
             }
-            if(correct === numRight) {
+            if (correct === numRight) {
                 numWrong++;
             }
-            if(numRight === wordArray.length) {
+            if (numRight === wordArray.length) {
                 myScore = myScore + 100
                 document.getElementById('scoreboard').innerHTML = "Score = " + myScore;
                 alert("You win! Click [New Game] to play again.");
             }
             guess[i].style.visibility = "hidden";
-            if(numWrong === 6) {
-                alert("You lose! Try again.");
+            if (numWrong === 1) {
+                hang1();
+            }
+            if (numWrong === 2) {
+                hang2();
+            }
+            if (numWrong === 3) {
+                hang3();
+            }
+            if (numWrong === 4) {
+                hang4();
+            }
+            if (numWrong === 5) {
+                hang5();
+            }
+            if (numWrong === 6) {
+                hang6();
+                setTimeout(alert("You lose! Try again."), 1000);
                 newGame();
             }
         })
@@ -82,62 +100,64 @@ function pickWord() {
 // //Display Hangman
 let canvas = document.getElementById("hangman");
 let hngmn = canvas.getContext("2d");
-hngmn.fillStyle = "white";
-hngmn.lineWidth = 4;
-hngmn.fillRect(0, 0, 400, 400);
-hngmn.beginPath();
-hngmn.moveTo(50, 350);
-hngmn.lineTo(50, 25);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(65, 350);
-hngmn.lineTo(65, 85);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(65, 64);
-hngmn.lineTo(65, 40);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(49, 25);
-hngmn.lineTo(250, 25);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(49, 40);
-hngmn.lineTo(86, 40);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(106, 40);
-hngmn.lineTo(250, 40);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(248, 25);
-hngmn.lineTo(248, 40);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(50, 80);
-hngmn.lineTo(100, 25);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(60, 90);
-hngmn.lineTo(111, 35);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(50, 80);
-hngmn.lineTo(60, 90);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(100, 25);
-hngmn.lineTo(111, 35);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(35, 350);
-hngmn.lineTo(365, 350);
-hngmn.stroke();
-hngmn.beginPath();
-hngmn.moveTo(225, 40);
-hngmn.lineTo(225, 100);
-hngmn.stroke();
-    
+function hangman() {
+    hngmn.fillStyle = "white";
+    hngmn.lineWidth = 4;
+    hngmn.fillRect(0, 0, 400, 400);
+    hngmn.beginPath();
+    hngmn.moveTo(50, 350);
+    hngmn.lineTo(50, 25);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(65, 350);
+    hngmn.lineTo(65, 85);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(65, 64);
+    hngmn.lineTo(65, 40);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(49, 25);
+    hngmn.lineTo(250, 25);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(49, 40);
+    hngmn.lineTo(86, 40);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(106, 40);
+    hngmn.lineTo(250, 40);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(248, 25);
+    hngmn.lineTo(248, 40);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(50, 80);
+    hngmn.lineTo(100, 25);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(60, 90);
+    hngmn.lineTo(111, 35);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(50, 80);
+    hngmn.lineTo(60, 90);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(100, 25);
+    hngmn.lineTo(111, 35);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(35, 350);
+    hngmn.lineTo(365, 350);
+    hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(225, 40);
+    hngmn.lineTo(225, 100);
+    hngmn.stroke();
+}
+
 
 //Display alphabet
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
@@ -148,42 +168,43 @@ const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 //Create functions for visuals of game
 
 function hang1() {
-	hngmn.beginPath();
-	hngmn.arc(225, 130, 30, 0, 2 * Math.PI);
-	hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.arc(225, 130, 30, 0, 2 * Math.PI);
+    hngmn.stroke();
 }
 
 function hang2() {
-	hngmn.beginPath();
-	hngmn.moveTo(225, 160);
-	hngmn.lineTo(225, 250);
-	hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(225, 160);
+    hngmn.lineTo(225, 250);
+    hngmn.stroke();
 }
 
 function hang3() {
-	hngmn.beginPath();
-	hngmn.moveTo(225, 195)
-	hngmn.lineTo(170, 158)
-	hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(225, 195)
+    hngmn.lineTo(170, 158)
+    hngmn.stroke();
 }
 
 function hang4() {
-	hngmn.beginPath();
-	hngmn.moveTo(225, 195)
-	hngmn.lineTo(280, 158)
-	hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(225, 195)
+    hngmn.lineTo(280, 158)
+    hngmn.stroke();
 }
 
 function hang5() {
-	hngmn.beginPath();
-	hngmn.moveTo(225,248)
-	hngmn.lineTo(170, 295)
-	hngmn.stroke();
+    hngmn.beginPath();
+    hngmn.moveTo(225, 248)
+    hngmn.lineTo(170, 295)
+    hngmn.stroke();
 }
 
 function hang6() {
-	hngmn.beginPath();
-	hngmn.moveTo(225, 195)
-	hngmn.lineTo(280, 158)
+    hngmn.beginPath();
+    hngmn.moveTo(225, 195)
+    hngmn.lineTo(280, 158)
     hngmn.stroke();
 }
+
